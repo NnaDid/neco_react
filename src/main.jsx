2c69975e2223c936be935208ef52729b01f0eab2
NnaDid <chrispneuma@gmail.com>
2023-11-20 18:04:55 +0100

    added routing and separated concerns

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
+
+
 import App from './App.jsx' 
+import Login from './components/Login.jsx'
+import Signup from './components/Signup.jsx'
+
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(     [
+  {  path: "/",  element: <App />, },
+  {  path: "/login",  element: <Login />, },
+  {  path: "/create-account",  element: <Signup />, },
+] );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-       <App />
+         <RouterProvider router={router} />
     </ChakraProvider> 
   </React.StrictMode>,
 )
