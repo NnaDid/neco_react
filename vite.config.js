22d94a0a1df1ee43c1d791e664da6d3284b43d1c
NnaDid <chrispneuma@gmail.com>
2023-11-22 18:16:21 +0100

    added osting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://beautifulcolourslaundry.com.ng/",
   plugins: [react()],
 })
